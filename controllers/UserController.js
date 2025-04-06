57e898c1273fd091693f3ebc67ed84ffde248fed
anjasmarats <atsakunprogramming@gmail.com>
2025-04-06 22:45:54 +0700

    membuat api get file user photo

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,12 +2,50 @@ const { app } = require('../server');
 const { User } = require('../models');
 const bcryptjs = require('bcryptjs');
 
+app.get('/api/user/photo', async (req, res) => {
+    try {
+        if (!req.headers.authorization) {
+            console.error("error get course user photo unauthorized\n\nreq.headers= ",req.headers.authorization);
+            return res.status(400).json();
+        }
+        const user = await User.findOne(
+            { where: { logind: req.headers.authorization.split(' ')[1] },attributes: ['photo'] }
+        );
+
+        if (!user) {
+            console.error("error get course user photo unauthorized\nisuserexist= ",user);
+            return res.status(400).json();
+        }
+
+        const photo = `/public/users/${user.photo}`;
+        return res.status(200).download(photo, {
+            headers: {
+                'Content-Type': 'image/*',
+                'Content-Disposition': `attachment; filename=${user.photo}`,
+            },
+        });
+    } catch (error) {
+        console.error("error server get user photo\n",error.message);
+        return res.status(500).json({ error: error.message });
+    }
+});
+
 app.post('/api/user', async (req, res) => {
     try {
         const { name, email, password, role } = req.body;
         if (!req.body||!name || !email || !password) {
-            console.log("error post user data tidak lengkap\nreq.body= ",req.body);
+            console.error("error post user data tidak lengkap\nreq.body= ",req.body);
             return res.status(400).json();
+        }
+        const user = User.findAll({
+            where:{
+                email
+            }
+        })
+
+        if (user) {
+            console.error("error post user, user sudah ada")
+            return res.status(400).json({message:"email"})
         }
         const logind = Array.from({length: 255}, () => Math.random().toString(36)[2]).join('');
         if (req.files && req.files.image) {
@@ -34,7 +72,7 @@ app.post('/api/user', async (req, res) => {
         const hashedPassword = await bcryptjs.hash(password, 10);
         return res.status(201).json({data:logind});
     } catch (error) {
-        console.log("error server post user\n",error.message);
+        console.error("error server post user\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -42,14 +80,14 @@ app.post('/api/user', async (req, res) => {
 app.get('/api/user/:id', async (req, res) => {
     try {
         if (!req.params.id || !req.headers.authorization) {
-            console.log("error get user data unauthorized\nreq.params= ",req.params,"req.headers= ",req.headers.authorization);
+            console.error("error get user data unauthorized\nreq.params= ",req.params,"req.headers= ",req.headers.authorization);
             return res.status(400).json();
         }
         const admin = await User.findOne(
             { where: { logind: req.headers.authorization.split(' ')[1],role:'admin' } }
         );
         if (!admin) {
-            console.log("error get user unauthorized\nadmin= ",admin);
+            console.error("error get user unauthorized\nadmin= ",admin);
             return res.status(404).json({ data:false });
         }
         // Find user by ID
@@ -58,7 +96,7 @@ app.get('/api/user/:id', async (req, res) => {
         );
         return res.status(200).json({users});
     } catch (error) {
-        console.log("error server get user\n",error.message);
+        console.error("error server get user\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -66,7 +104,7 @@ app.get('/api/user/:id', async (req, res) => {
 app.put('/api/user/:id', async (req, res) => {
     try {
         if (!req.params.id || !req.headers.authorization) {
-            console.log("error put user unauthorized\nreq.params= ",req.params,"\nreq.headers= ",req.headers.authorization);
+            console.error("error put user unauthorized\nreq.params= ",req.params,"\nreq.headers= ",req.headers.authorization);
             return res.status(400).json();
         }
         const { name, email, password, role } = req.body;
@@ -78,7 +116,7 @@ app.put('/api/user/:id', async (req, res) => {
         const cekPassword = await bcryptjs.compare(password, isUserExist?isUserExist.password:"");
         
         if (!req.body || !name || !email || !password || !req.headers.authorization || !isUserExist || !cekPassword ) {
-            console.log("error put user data tidak lengkap\nreq.body= ",req.body,"cekpassword= ",cekPassword,"isuserexist= ",isUserExist);
+            console.error("error put user data tidak lengkap\nreq.body= ",req.body,"cekpassword= ",cekPassword,"isuserexist= ",isUserExist);
             return res.status(400).json();
         }
         const hashedPassword = await bcryptjs.hash(password, 10);
@@ -99,7 +137,7 @@ app.put('/api/user/:id', async (req, res) => {
         // Find user by ID
         return res.status(200).json();
     } catch (error) {
-        console.log("error server put user\n",error.message);
+        console.error("error server put user\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -107,14 +145,14 @@ app.put('/api/user/:id', async (req, res) => {
 app.delete('/api/user/:id', async (req, res) => {
     try {
         if (!req.params.id || !req.headers.authorization) {
-            console.log("error delete user data tidak lengkap\nreq.params= ",req.params,"\nreq.headers= ",req.headers.authorization);
+            console.error("error delete user data tidak lengkap\nreq.params= ",req.params,"\nreq.headers= ",req.headers.authorization);
             return res.status(400).json();
         }
         const admin = await User.findOne(
             { where: { logind: req.headers.authorization.split(' ')[1],role:'admin' } }
         );
         if (!admin) {
-            console.log("error delete user unauthorized\nadmin= ",admin);
+            console.error("error delete user unauthorized\nadmin= ",admin);
             return res.status(404).json();
         }
         // Find user by ID
@@ -123,7 +161,7 @@ app.delete('/api/user/:id', async (req, res) => {
         );
         return res.status(200).json();
     } catch (error) {
-        console.log("error server delete user\n",error.message);
+        console.error("error server delete user\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -134,14 +172,14 @@ app.get('/api/users', async (req, res) => {
             { where: { role: 'admin',logind:req.headers.authorization.split(' ')[1] } }
         );
         if (!req.headers.authorization ||!admin) {
-            console.log("error get users data tidak lengkap\nreq.headers= ",req.headers,"admin= ",admin);
+            console.error("error get users data tidak lengkap\nreq.headers= ",req.headers,"admin= ",admin);
             return res.status(400).json({ error: 'Admin not found' });
         }
         // Find all users
         const users = await User.findAll();
         return res.status(200).json(users);
     } catch (error) {
-        console.log("error server get users\n",error.message);
+        console.error("error server get users\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -149,19 +187,19 @@ app.get('/api/users', async (req, res) => {
 app.get('/api/auth', async (req, res) => {
     try {
         if (!req.headers.authorization) {
-            console.log("error get auth data tidak lengkap\nreq.headers= ",req.headers.authorization);
+            console.error("error get auth data tidak lengkap\nreq.headers= ",req.headers.authorization);
             return res.status(400).json();
         }
         const user = await User.findOne(
             { where: { logind: req.headers.authorization } }
         );
         if (!user) {
-            console.log("error get auth unauthorized\nuser= ",user);
+            console.error("error get auth unauthorized\nuser= ",user);
             return res.status(404).json({ data:false });
         }
         return res.status(200).json({data:true,admin:user.role==='admin'});
     } catch (error) {
-        console.log("error server get auth\n",error.message);
+        console.error("error server get auth\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
@@ -169,7 +207,7 @@ app.get('/api/auth', async (req, res) => {
 app.get('/api/user', async (req, res) => {
     try {
         if (!req.headers.authorization) {
-            console.log("error get user data tidak lengkap\nreq.headers= ",req.headers.authorization);
+            console.error("error get user data tidak lengkap\nreq.headers= ",req.headers.authorization);
             return res.status(400).json();
         }
         const user = await User.findOne(
@@ -179,12 +217,12 @@ app.get('/api/user', async (req, res) => {
             }
         );
         if (!user) {
-            console.log("error get user unauthorized\nuser= ",user);
+            console.error("error get user unauthorized\nuser= ",user);
             return res.status(404).json({ data:false });
         }
         return res.status(200).json({user});
     } catch (error) {
-        console.log("error server get user\n",error.message);
+        console.error("error server get user\n",error.message);
         return res.status(500).json({ error: error.message });
     }
 });
